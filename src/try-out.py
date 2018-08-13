3c58ac0bff5161d184ac4722be128da56960fb2e
DShalygin <DShalygin@it.ru>
2018-08-13 16:58:03 +0700

    with ?

--- src/try-out.py
+++ src/try-out.py
@@ -43,8 +43,8 @@ def login():
         return "login NOT POST"
         # return show_the_login_form()
 
-# with app.test_request_context():
-#     print(url_for('index'))
-#     print(url_for('path'))
-#     print(url_for('path', next='/'))
-#     print(url_for('profile', username='John Doe'))
+with app.test_request_context('/hello', method='POST'):
+    # now you can do something with the request until the
+    # end of the with block, such as basic assertions:
+    assert request.path == '/hello'
+    assert request.method == 'POST'
