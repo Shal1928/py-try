867806a77b2577a060d5d89a224ad66d72ac51a2
Shal1928 <Shal1928@yandex.ru>
2018-08-14 23:07:04 +0700

    worked Y weather Request

--- src/async-try.py
+++ src/async-try.py
@@ -1,17 +1,36 @@
-import aiohttp
-import asyncio
+import json
+from urllib import request
+from urllib.request import urlopen
+
+# Address for sending JSON requests
+from flask import app, Flask
+
+app = Flask(__name__)
+
+url = 'https://api.weather.yandex.ru/v1/informers?lat=55.75396&lon=37.620393'
+
+# data for OAuth authorization
+token = 'e4d3b4d2a7444fa384a18cda5cd1c8d9'
+
+# Yandex.Direct login
+login = 'agrom'
+# X-Yandex-API-Key
+# input data structure (dictionary)
+data = {
+   'method': 'GetClientInfo',
+   'token': token,
+   'locale': 'ru',
+   'param': [login]
+}
 
 
-async def fetch(session, url):
-    async with session.get(url) as response:
-        return await response.text()
+@app.route('/')
+def index():
+    req = request.Request(url, headers={"X-Yandex-API-Key" : "02d3e606-cc1e-4c83-a3a5-55add3fa5f1b"})
+    with request.urlopen(req) as response:
+        print(response.read().decode('utf8'))
+
+    # output the result
+    return 'Index Page'
 
 
-async def main():
-    async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'http://python.org')
-        print(html)
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
