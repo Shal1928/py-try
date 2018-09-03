7c315927eb466f0ecdb67335ac573fa6e9870df0
DShalygin <DShalygin@it.ru>
2018-09-03 12:13:42 +0700

    Stack impl

--- src/data-struct/Stack.py
+++ src/data-struct/Stack.py
@@ -2,8 +2,27 @@ class StackTry:
     def __init__(self):
         self.list = []
 
-    def push(self, newElement):
-        self.list.append(newElement)
+    def push(self, new_element):
+        self.list.append(new_element)
 
     def pop(self):
-        self.list.pop()
+        if not self.list:
+            return
+        else:
+            self.list.pop()
+
+    def __repr__(self):
+        return ', '.join(str(i) for i in self.list)
+
+
+stack = StackTry()
+print('1 stack = {0}'.format(stack))
+stack.push(1)
+stack.push(2)
+print('2 stack = {0}'.format(stack))
+stack.pop()
+print('3 stack = {0}'.format(stack))
+stack.pop()
+stack.pop()
+stack.pop()
+print('4 stack = {0}'.format(stack))
