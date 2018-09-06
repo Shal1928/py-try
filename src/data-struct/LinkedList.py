ff73a7c23df295a6b9e51cf461d8907d4d55a48f
DShalygin <DShalygin@it.ru>
2018-09-06 22:25:11 +0700

    LinkedList useScript

--- src/data-struct/LinkedList.py
+++ src/data-struct/LinkedList.py
@@ -12,17 +12,24 @@ class LinkedList:
         self.list = []
         self.index = 0
 
-    # def add(self, value, index):
-    #     self.list.append(Node(value, index))
-
     def add(self, value):
         self.index += 1
-        self.add(value, self.index)
+        self.list.append(Node(value, self.index))
+
+    def insert(self, value, index):
+
 
     def remove(self, index):
         if index <= self.index:
             print("value: {0} is deleted".format(self.list[index]))
             self.list.pop(index)
+        else: raise IndexError("Index {0} is out of range!".format(index))
 
     def __repr__(self):
-        return ', '.join(str(i) for i in self.list)
+        return ", ".join(str(i) for i in self.list)
+
+linkedList = LinkedList()
+print("1 stack = {0}".format(linkedList))
+linkedList.add(1)
+linkedList.add(2)
+print("2 stack = {0}".format(linkedList))
