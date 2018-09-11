02c9831601b61f20a42aa4a809aaf6a057a49921
Shal1928 <Shal1928@yandex.ru>
2018-09-11 22:32:47 +0700

    LinkedList insert impl COMPLETE

--- src/data-struct/LinkedList.py
+++ src/data-struct/LinkedList.py
@@ -1,6 +1,3 @@
-from operator import attrgetter
-
-
 class Node:
     def __init__(self, data=None, next=None):
         self.data = data
@@ -20,23 +17,19 @@ class LinkedList:
         self.list.append(Node(value, self.index))
 
     def insert(self, value, index):
-        # for i in range(index, self.index):
-        #     self.list[i].next += 1
-        # self.list.append(Node(value, index + 1))
-        # self.list = sorted(self.list, key=attrgetter('next'))
-        self.index += 1
-        self.list.append(Node())
-        for i in self.list[::-1]:
-            print(i)
-            print(self.list[i])
-            # if i==index:
-            #     self.list[i]=Node(value, self.index)
-            # else:
-            #     self.list[i]=v
+        self.add(None)
+        for node in self.list[::-1]:
+            if self.index == node.next:
+                continue
+            i = self.list.index(node)
+            node.next += 1
+            self.list[i + 1] = node
+            if i == index:
+                self.list[i] = Node(value, i+1)
+                break
 
     def remove(self, index):
         if index <= self.index:
-            print("value: {0} is deleted".format(self.list[index]))
             self.list.pop(index)
         else:
             raise IndexError("Index {0} is out of range!".format(index))
@@ -51,4 +44,6 @@ linkedList.add(1)
 linkedList.add(2)
 print("2 stack = {0}".format(linkedList))
 linkedList.insert(1.5, 1)
+print("3 stack = {0}".format(linkedList))
+linkedList.insert(1.4, 1)
 print("3 stack = {0}".format(linkedList))
