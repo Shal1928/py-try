d9375c8bfc2ae0f910b307d68a342919d12843fd
DShalygin <DShalygin@it.ru>
2018-09-14 11:55:26 +0700

    key is conditions

--- src/workarounds/Switch.py
+++ src/workarounds/Switch.py
@@ -7,3 +7,11 @@ s = {
     value <= 20: "<=20 2",
     }[True]
 print(s)
+s = {
+    value <= 20: "<=20 0",
+    5 <= value <= 10: "5-10",
+    value <= 20: "<=20",
+    value != 0: "!=0",
+    value <= 20: "<=20 2",
+    }[value != 0]
+print(s)
