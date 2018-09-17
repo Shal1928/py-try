148cb2448e0792fca4263c194ea9cc019a2e8eb1
DShalygin <DShalygin@it.ru>
2018-09-17 11:57:42 +0700

    first task, other variants

--- src/courses/Intro-1.1.py
+++ src/courses/Intro-1.1.py
@@ -2,3 +2,19 @@ result = 0
 for i in range(int(input())):
     result += int(input())
 print(result)
+
+# import sys
+#
+#
+# n = sys.stdin.readline()
+# data = map(int, sys.stdin.read().split('\n'))
+# print(sum(data))
+
+# print(sum([int(input()) for i in range(int(input()))]))
+
+# n, res = int(input()), 0
+# while n:
+#     res += int(input())
+#     n -= 1
+#
+# print(res)
