096a36b6b2f4a26b107f59a7694d0eb72456c77a
DShalygin <DShalygin@it.ru>
2018-09-27 11:31:50 +0700

    fix 1 and True situation

--- src/courses/Types-1.2.py
+++ src/courses/Types-1.2.py
@@ -1,7 +1,7 @@
-objects = [1, 2, 3, 3, 3, 'Hello', {'4'}, 0]
+objects = [1, 2, 3, 3, 3, 'Hello', {'4'}, 0, True]
 s = set()
 for obj in objects:
-    s.add(objects.index(obj))
+    s.add(id(obj))
 print(s.__len__())
 
 
