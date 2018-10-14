3fccfc16cca1a9531d331ce96d88e0e09963d660
Shal1928 <Shal1928@yandex.ru>
2018-10-14 22:48:50 +0700

    Отредактировал порядок импортов

--- src/ml/iris/data-learning.py
+++ src/ml/iris/data-learning.py
@@ -2,11 +2,10 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import mglearn
-import matplotlib.pyplot as plt
-from sklearn.neighbors import KNeighborsClassifier
-
-plt.rc('font', family='Verdana')
 from IPython.display import display
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+plt.rc('font', family='Verdana')
 iris_dataset = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
 knn = KNeighborsClassifier(n_neighbors=1)
