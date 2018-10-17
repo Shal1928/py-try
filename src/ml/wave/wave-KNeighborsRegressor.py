acf58e321e222598fdcccc3cc4d637c610d177a7
Shal1928 <Shal1928@yandex.ru>
2018-10-17 23:10:58 +0700

    wave regression KNeighbors analyze

---
+++ src/ml/wave/wave-KNeighborsRegressor.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import mglearn
+from IPython.display import display
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
+import matplotlib.pyplot as plt
+X, y = mglearn.datasets.make_wave(n_samples=40)
+plt.plot(X, y, 'o')
+plt.ylim(-3, 3)
+plt.xlabel("Признак")
+plt.ylabel("Целевая переменная")
+plt.show()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+reg = KNeighborsRegressor(n_neighbors=3)
+reg.fit(X_train, y_train)
+print("Прогнозы для тестового набора:\n{}".format(reg.predict(X_test)))
+print("R^2 на тестовом наборе: {:.2f}".format(reg.score(X_test,y_test)))
