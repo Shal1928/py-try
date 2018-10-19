22fbb0a865cd4169a1c00826465f586426cc6a31
Shal1928 <Shal1928@yandex.ru>
2018-10-19 23:15:42 +0700

    OLR, ridge examples

---
+++ src/ml/wave/wave-linear-regression-analyze.py
@@ -0,0 +1,11 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import mglearn
+from IPython.display import display
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
+from pandas import np
+import matplotlib.pyplot as plt
+mglearn.plots.plot_linear_regression_wave()
+plt.show()
