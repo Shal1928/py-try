3bd7d415601661cfad3975c460c305da655e1149
Shal1928 <Shal1928@yandex.ru>
2018-11-12 20:58:56 +0700

    Decision-trees graphviz

---
+++ src/ml/decision-trees/intro.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import load_iris
+from sklearn.datasets import make_blobs
+from mglearn.datasets import load_extended_boston
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import mglearn
+from IPython.display import display
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from pandas import np
+import graphviz
+import matplotlib.pyplot as plt
+mglearn.plots.plot_animal_tree()
+plt.show()
