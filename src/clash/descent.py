b249395b0f8e8e2f187e22ed1580bf1c6ee90afe
Shal1928 <Shal1928@yandex.ru>
2020-02-04 13:14:20 +0700

    descent

---
+++ src/clash/descent.py
@@ -0,0 +1,25 @@
+import sys
+import math
+
+# The while loop represents the game.
+# Each iteration represents a turn of the game
+# where you are given inputs (the heights of the mountains)
+# and where you have to print an output (the index of the mountain to fire on)
+# The inputs you are given are automatically updated according to your last actions.
+
+
+# game loop
+m = []
+while True:
+    for i in range(8):
+        m.append(int(input()))
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr)
+
+    # The index of the mountain to fire on.
+    print(m.index(max(m)))
+    m = []
+
+# best solution:
+# while True:
+#     print(max([(int(input()),x) for x in range(8)])[1])
