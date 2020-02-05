768d10c26abd78ee4cd61a06bd271c0ea2c0db1d
dshalygin <DShalygin@it.ru>
2020-02-05 15:59:37 +0700

    clash middlepoint

--- src/clash/TEST.py
+++ src/clash/TEST.py
@@ -1,3 +1,5 @@
 import sys
 import math
 import re
+
+print('{0:g}'.format(2.551))
---
+++ src/clash/middlepoint.py
@@ -0,0 +1,3 @@
+x_1, y_1 = [int(i) for i in input().split()]
+x_2, y_2 = [int(i) for i in input().split()]
+print('{0:g}'.format((x_1 + x_2) / 2) + ' ' + '{0:g}'.format((y_1 + y_2) / 2))
